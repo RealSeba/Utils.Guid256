7e75776fd145a71a423f9c6b008e291c9352df70
Sebastian Sega <Sebastian.Sega@drheuer.de>
2025-02-26 00:10:34 +0100

    added unit tests for Dictionary Value and Key serialization/deseraialization

--- Utils.Guid256.XUnitTest/JsonUnitTest.cs
+++ Utils.Guid256.XUnitTest/JsonUnitTest.cs
@@ -22,5 +22,39 @@ namespace Utils.Guid256.XUnitTest
 
             Assert.Equal(guid1HexString, guid2HexString);
         }
+
+        [Fact]
+        public void NewGuid256_JSON_SerializeDeserilize_DictionaryValue()
+        {
+            //arrange
+            var guid1 = Guid256.NewGuid256();
+            
+            Dictionary<string, Guid256> dict = new Dictionary<string, Guid256>();
+            dict.Add("guid1", guid1);
+
+            string json = JsonSerializer.Serialize<Dictionary<string, Guid256>>(dict);
+            Dictionary<string, Guid256> dictDeser = JsonSerializer.Deserialize<Dictionary<string, Guid256>>(json);
+
+            //the first value guid256 string should be the same as the second one
+            Assert.Equal(guid1.ToString(), dictDeser["guid1"].ToString());
+        }
+
+
+        [Fact]
+        public void NewGuid256_JSON_SerializeDeserilize_DictionaryKey()
+        {
+            //arrange
+            var guid1 = Guid256.NewGuid256();
+
+            Dictionary<Guid256, string> dict = new Dictionary<Guid256, string>();
+            dict.Add(guid1, "guid1");
+
+            string json = JsonSerializer.Serialize<Dictionary<Guid256, string>>(dict);
+            Dictionary<Guid256, string> dictDeser = JsonSerializer.Deserialize<Dictionary<Guid256, string>>(json);
+
+            //the first value guid256 string should be the same as the second one
+            Assert.Equal(dict.Keys.First().ToString(), dictDeser.Keys.First().ToString());
+        }
+
     }
 }
